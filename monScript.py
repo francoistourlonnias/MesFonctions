52fa3f20417a77c10dcc2a68ecfb442347f17d2f
Francois <yftourlonni.as@gmail.com>
2019-05-10 22:38:40 +0200

    Ajout d'un nouveau fichier au repo

---
+++ monScript.py
@@ -0,0 +1,25 @@
+print ('Hoo hoo World')
+x = "Bonjour"
+y=x.upper()
+print (x) 
+print (y)
+x = 32
+nom = "John"
+print (nom , " a " , x , " ans")
+
+propGC = (4500.0 + 2575)/14800
+print ("La proportion de GC est", propGC)
+#print "La proportion de GC est %.2f" % propGC
+animaux = ["girafe","tigre","singe","souris"]
+for i in animaux:
+    print (i)
+print ("*********************")    
+for i in range(4):
+    print (animaux[i])
+print ("*********************")   
+for i in range(len(animaux)):
+    print (animaux[i])
+print ("*********************")   
+
+#http://python.developpez.com/tutoriels/cours-python-uni-paris7/
+#VB comparaison
