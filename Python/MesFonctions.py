626075d0546d708303756fa0d901b9ea797b731a
Francois <yftourlonni.as@gmail.com>
2019-05-10 22:15:19 +0200

    commit essavaleur ... 10

--- Python/MesFonctions.py
+++ Python/MesFonctions.py
@@ -3,7 +3,7 @@
 import os
 def table(nb, max=10):
     """Fonction affichant la table de multiplication par nb de
-    1 * nb jusqu'à max * nb"""
+    1 * nb jusqu'ï¿½ max * nb"""
     i = 0
     while i < max:
         print(i + 1, "*", nb, "=", (i + 1) * nb)
@@ -11,5 +11,5 @@ def table(nb, max=10):
 
 # test de la fonction table
 if __name__ == "__main__" :
-    table(7)
+    table(10)
     os.system("pause")
