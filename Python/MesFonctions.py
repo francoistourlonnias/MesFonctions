f06c0867fb6846fe5ecdef88425f524bacf4deaa
Francois <yftourlonni.as@gmail.com>
2019-05-29 21:37:55 +0200

    retrait d'un accnt

--- Python/MesFonctions.py
+++ Python/MesFonctions.py
@@ -3,7 +3,7 @@
 import os
 def table(nb, max=10):
     """Fonction affichant la table de multiplication par nb de
-    1 * nb jusqu'� max * nb"""
+    1 * nb jusqu'a max * nb"""
     i = 0
     while i < max:
         print(i + 1, "*", nb, "=", (i + 1) * nb)
